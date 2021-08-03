1fca39a0d983911767cb72d7358a3162a8026170
Cody West <cww2697@gmail.com>
2021-08-03 03:37:57 +0000

    Add make files

---
+++ CMakeFiles/CollatzConjecture.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CollatzConjecture.dir/main.c.o"
+  "CollatzConjecture"
+  "CollatzConjecture.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CollatzConjecture.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
